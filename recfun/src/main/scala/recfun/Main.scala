d6e7563042eac46d947accfd32a3456cb51db8d4
August <august@postech.ac.kr>
2021-09-09 22:25:40 +0900

    Made Exercise 1

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,21 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = 
+    if (c == 0 || c == r) 1 else pascal(c-1, r-1) + pascal(c, r-1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+    /*
+  def balance(chars: List[Char]): Boolean = {
+   def balanceCounter(open: Int, chars: List[Char]): Boolean =
+     if (chars.isEmpty)
 
+  }
+ */
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  //def countChange(money: Int, coins: List[Int]): Int = ???
 }
